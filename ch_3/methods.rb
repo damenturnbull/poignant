c499d5a3727e2216dda3a7ddc9ee16adfe4857e1
Damen Turnbull <info@fando.com.au>
2015-01-30 08:23:25 +1100

    Beginnings to chapter 3

---
+++ ch_3/methods.rb
@@ -0,0 +1,4 @@
+# In Ruby, everthing has access to methods, including numbers!
+10.times { puts "One pint of Pale Ale please!" }
+
+# puts 10.methods
---
+++ ch_3/unless.rb
@@ -0,0 +1,3 @@
+# Ruby effectively uses question marks to increase readibility
+craft_beer = "craft beer is made from high quality hops, barley and yeast"
+puts craft_beer unless craft_beer.include? "vodka"

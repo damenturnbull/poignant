bd4a5179e2db18747dc7496aa0088e28649c9091
Damen Turnbull <info@fando.com.au>
2015-02-02 13:46:29 +1100

    Exploring case/when statements

---
+++ ch_5/case_when.rb
@@ -0,0 +1,21 @@
+# Similar to a switch statement. a 'case' or 'case when' declares
+def beers_consumed( num_beers )
+	case num_beers
+	when 0
+		"very sober indeed." # return statement is implied
+	when 1..2
+		"slightly tipsy."
+	when 3..4
+		"tipsy."
+	when 5..10
+		"drunk."
+	else
+		"wasted. You'll be feeling it tomorrow..."
+	end
+end
+
+# Output
+puts "Enter number of beers consumed:"
+beer_count = gets.strip
+# Use .to_i to convert string to integer
+puts "You've had " + beer_count + " beers? Looks like you're " + beers_consumed( beer_count.to_i )
